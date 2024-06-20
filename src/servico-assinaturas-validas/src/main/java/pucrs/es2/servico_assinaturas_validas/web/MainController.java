59a2b94ac9b0b37d95e47cbd0a860f1fb8261b7a
Ricardo Süffert <ricardo.suffert@edu.pucrs.br>
2024-06-20 02:38:39 -0300

    remove debug prints

--- src/servico-assinaturas-validas/src/main/java/pucrs/es2/servico_assinaturas_validas/web/MainController.java
+++ src/servico-assinaturas-validas/src/main/java/pucrs/es2/servico_assinaturas_validas/web/MainController.java
@@ -23,27 +23,20 @@ public class MainController {
     @CrossOrigin("*")
     public ResponseEntity<Boolean> verificarValidade(@PathVariable Long codass) {
         boolean valid;
-        System.out.println("Requisição recebida: " + codass);
-        
         Assinatura ass = CacheService.getAssinaturaByCodigo(codass);
-        if (ass != null) { // cache hit
+
+        if (ass != null) // cache hit
             valid = !LocalDate.now().isAfter(ass.fimVigencia());
-            System.out.println("Cache hit! Valid = " + valid);
-        }
         else { // cache miss (fazer RPC ao servico-cadastramento para saber a validade da assinatura)
-            System.out.println("Cache miss! Enviando requisição RPC...");
             Object responseObj = rabbitTemplate.convertSendAndReceive(RabbitMQConfig.DIRECTEXCHANGENAME, "rpc", codass);
             if (responseObj instanceof LocalDate) {
                 LocalDate response = (LocalDate) responseObj;
                 // adicionar na cache e verificar validade da assinatura
                 CacheService.codigosAssinaturasValidas.add(new Assinatura(codass, response));
                 valid = !LocalDate.now().isAfter(response);
-                System.out.println("Assinatura existente! Valid = " + valid);
             }
-            else {// assinatura nao existe 
+            else // assinatura nao existe 
                 valid = false;
-                System.out.println("Assinatura não existente! Valid = " + valid);
-            }
         }
 
         return ResponseEntity.ok(valid);
